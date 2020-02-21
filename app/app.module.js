7cc938d99e45607c48b77044766e74af212238e5
afif0808 <afif.panai@gmail.com>
2020-02-21 08:28:38 +0700

    another commit

--- app/app.module.js
+++ app/app.module.js
@@ -2,5 +2,8 @@ const app = angular.module('cusApp',['ngRoute'])
 
 app.controller('cusMainController',function($scope,$http){
   $scope.customersAPPURL = "http://localhost:321/#!/"
-  $scope.customersAPIURL = "http://localhost:444/api/customers"
+
+  window.onload = function() {
+    $scope.getCustomers($scope.customersAPIURL)
+  }
 })
--- app/components/customer/customerController.js
+++ app/components/customer/customerController.js
@@ -1,19 +1,17 @@
 app.controller('customerController',function($http,$scope){
- window.onload = function() {
-   $scope.getCustomers($scope.customersAPIURL)
- }
-
+  $scope.$on('$routeChangeSuccess', function($event, next, current) {
+    $scope.getCustomers($scope.customersAPIURL)
+   });
  $scope.customers = []
  $scope.nextCustomersURL = ""
  $scope.previousCustomersURL = ""
  $scope.customersLimit = 4;
- $scope.customersAPIURL = $scope.$parent.customersAPIURL + "?limit=" + $scope.customersLimit
+ $scope.customersAPIURL =  "http://localhost:444/api/customers?limit=" + $scope.customersLimit
  $scope.customersAPPURL = $scope.$parent.customersAPPURL
  /**
    * Get customer resource from customers API
    * @param {string} customer - customer resource API url
  */
- $scope.res
  $scope.getCustomers = function(customerURL) {
    $http.get(customerURL).then(function(res){
      $scope.customers = res.data.result
@@ -21,6 +19,9 @@ app.controller('customerController',function($http,$scope){
      $scope.previousCustomersURL = res.data.previous
    })
  }
+
+ // make getCustomers() function accessible to parent controller
+ $scope.$parent.getCustomers = $scope.getCustomers
 
  /**
    * Search for customers from customers API with 'getCustomers()' function
